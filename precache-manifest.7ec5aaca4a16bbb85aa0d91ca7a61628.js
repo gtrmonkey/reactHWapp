afeeffa49dc11f8275ce21c33fbf389314da2775
gtrmonkey <gtrmonkey@gmail.com>
2019-01-31 21:29:54 -0500

    Updates

---
+++ precache-manifest.7ec5aaca4a16bbb85aa0d91ca7a61628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00b9b2d5a70be82f435a",
+    "url": "/reactHWapp/static/js/runtime~main.00b9b2d5.js"
+  },
+  {
+    "revision": "9b5cd60cde014c8e978d",
+    "url": "/reactHWapp/static/js/main.9b5cd60c.chunk.js"
+  },
+  {
+    "revision": "af2c9dd9ba61e82900a1",
+    "url": "/reactHWapp/static/js/1.af2c9dd9.chunk.js"
+  },
+  {
+    "revision": "9b5cd60cde014c8e978d",
+    "url": "/reactHWapp/static/css/main.47e819bf.chunk.css"
+  },
+  {
+    "revision": "b0c55ef6203cb2d0117f080868bf1feb",
+    "url": "/reactHWapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactHWrepiceapp/precache-manifest.9188198a026e21481464dd6fe076e00a.js"
+  "/reactHWapp/precache-manifest.7ec5aaca4a16bbb85aa0d91ca7a61628.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactHWrepiceapp/index.html", {
+workbox.routing.registerNavigationRoute("/reactHWapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
